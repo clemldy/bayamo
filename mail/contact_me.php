164c348cd1bb55a6d9d6b9ff1d02ccb63589c94d
bob ricard <clement.ledey@gmail.com>
2018-11-26 14:07:11 +0100

    add contact email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,18 +9,18 @@ if(empty($_POST['name'])      ||
    echo "Aucun argument fourni!";
    return false;
    }
-   
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+
 // Create the email and send the message
-$to = 'contact@comptoirrhum.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contact@casa-bayamo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Message de :  $name";
 $email_body = "Vous avez reçu un message du formulaire de contact\n\n"."Détails :\n\nNom : $name\n\nEmail : $email_address\n\nTéléphone: $phone\n\nMessage :\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
 ?>
